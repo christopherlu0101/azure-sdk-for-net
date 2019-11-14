a9ad4a26892ae661f4a6574ffc30623fe275950c
Christopher Lu <falu@microsoft.com>
2019-11-14 19:58:49 +0800

    adjust AnalyzeUrlRequest json property to satisfy API call

--- sdk/cognitiveservices/FormRecognizer/src/Customizations/Models/AnalyzeUrlRequest.cs
+++ sdk/cognitiveservices/FormRecognizer/src/Customizations/Models/AnalyzeUrlRequest.cs
@@ -1,11 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 
 using System;
+using Newtonsoft.Json;
 
 namespace Microsoft.Azure.CognitiveServices.FormRecognizer.Models
 {
     public class AnalyzeUrlRequest
     {
+        [JsonProperty(PropertyName = "url")]
         public Uri Source { get; set; }
     }
 }
