17117595c39c5dea2e42209b3bbf7da16420289f
Christopher Lu <falu@microsoft.com>
2019-12-24 17:46:28 +0800

    take home

---
+++ sdk/cognitiveservices/FormRecognizer/src/FormRecognizer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Core.Pipeline;
+
+namespace Microsoft.Azure.CognitiveServices.Vision.FormRecognizer
+{
+    public class FormRecognizerClient
+    {
+        public Credential Credentials { get; set; }
+        private HttpPipeline _httpPipeline;
+        private RequestUriBuilder _uriBuilder;
+
+        public FormRecognizerClient(string endpoint, string APIKey)
+        {
+            Credentials = new Credential(APIKey);
+            _httpPipeline = new HttpPipeline(new HttpClientTransport());
+            _uriBuilder = new RequestUriBuilder();
+            _uriBuilder.Reset(new Uri(endpoint));
+        }
+
+        public async Task<string> StartAnalyzeReceiptAsync(Stream fileStream, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // _uriBuilder.AppendPath("prebuilt/receipt/analyze", false);
+            using (var request = _httpPipeline.CreateRequest())
+            {
+                request.Method = RequestMethod.Post;
+                request.Uri = _uriBuilder;
+                if (Credentials != null)
+                {
+                    await Credentials.ProcessHttpRequestAsync(request, cancellationToken);
+                }
+                request.
+                request.Content = RequestContent.Create(fileStream);
+                var response = await _httpPipeline.SendRequestAsync(request, cancellationToken).ConfigureAwait(false);
+                if (response.Status == 202)
+                {
+                    string id;
+                    response.Headers.TryGetValue("Operation-Location", out id);
+                    return id;
+                }
+                else
+                {
+                    throw await response.CreateRequestFailedExceptionAsync();
+                }
+            }
+        }
+
+        //public async Task<Response<AnalyzeResult>> GetReceiptAnalyzeResultAsync()
+        //{
+
+        //}
+
+
+    }
+
+    public class AnalyzeResult
+    {
+
+    }
+
+    public class Credential
+    {
+        private readonly string _subscriptionKey;
+        private const string ApiKeyHeader = "Ocp-Apim-Subscription-Key";
+
+        public Credential(string subscriptionKey)
+        {
+            _subscriptionKey = subscriptionKey;
+        }
+
+        public Task ProcessHttpRequestAsync(Request request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+            request.Headers.Add(ApiKeyHeader, this._subscriptionKey);
+            return Task.FromResult<object>(null);
+        }
+    }
+}
--- sdk/cognitiveservices/FormRecognizer/tests/FormRecognizerTests.cs
+++ sdk/cognitiveservices/FormRecognizer/tests/FormRecognizerTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Azure.CognitiveServices.FormRecognizer;
-using Microsoft.Azure.CognitiveServices.FormRecognizer.Models;
+﻿using Microsoft.Azure.CognitiveServices.Vision.FormRecognizer;
 using Microsoft.Azure.Test.HttpRecorder;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
 using Newtonsoft.Json;
@@ -9,14 +8,20 @@ using System.IO;
 
 namespace FormRecognizerSDK.Tests
 {
-    public class FormRecognizerTests : BaseTests
+    public class FormRecognizerTests
     {
         [Fact]
-        public void VerifyFormClientObjectCreation()
+        public void test()
         {
-            var client = GetFormRecognizerClient(null);
+            var endpoint = "https://westus2.ppe.cognitiveservices.azure.com/formrecognizer/v2.0-preview/prebuilt/receipt/analyze";
+            var apiKey = "184654c847d54432b8301a4b76f63045";
+            var client = new FormRecognizerClient(endpoint, apiKey);
 
-            Assert.True(client.GetType() == typeof(FormRecognizerClient));
+            using (FileStream stream = new FileStream(@"D:/Data/Receipt_000_000.jpg", FileMode.Open))
+            {
+                var id = client.StartAnalyzeReceiptAsync(stream).Result;
+
+            }
         }
     }
 }
